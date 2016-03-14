b4e3303dc71e2ddd690a338ea553eb1c69c459d3
Tony Bowden <tony@tmtm.com>
2016-03-14 23:09:08 +0000

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,22 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'wikidata/fetcher'
+
+es_2011 = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://es.wikipedia.org/wiki/Composici%C3%B3n_de_la_Asamblea_Nacional_de_Venezuela_per%C3%ADodo_2011-2016',
+  after: '//span[@id="Diputados_por_part.C3.ADdo"]',
+  before: '//span[@id="Hist.C3.B3rico_Directiva"]',
+  xpath: '//table[.//th[.="Diputado"]]//td[2]//a[not(@class="new")]/@title',
+) 
+
+es_2016 = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://es.wikipedia.org/wiki/Composici%C3%B3n_de_la_Asamblea_Nacional_de_Venezuela_per%C3%ADodo_2016-2021',
+  after: '//span[@id="Diputados_por_partido"]',
+  before: '//span[@id="Directiva"]',
+  xpath: '//table[.//th[contains(.,"Diputado")]]//td[2]//a[not(@class="new")]/@title',
+  debug: true,
+) 
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { es: es_2011 | en_2016 }, output: false)
+
