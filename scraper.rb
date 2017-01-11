20c3e1eb699f119578cbfeb36006f20fec492e82
Tony Bowden <tony@tmtm.com>
2017-01-11 08:34:02 +0000

    Handle Wikipedia heading rename

--- scraper.rb
+++ scraper.rb
@@ -21,7 +21,7 @@ es_2015 = EveryPolitician::Wikidata.wikipedia_xpath(
 es_2016 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://es.wikipedia.org/wiki/Composici%C3%B3n_de_la_Asamblea_Nacional_de_Venezuela_per%C3%ADodo_2016-2021',
   after: '//span[@id="Diputados_por_partido"]',
-  before: '//span[@id="Directiva"]',
+  before: '//span[@id="Directiva.2C_secretar.C3.ADa_y_jefaturas"]',
   xpath: '//table[.//th[contains(.,"Diputado")]]//td[2]//a[not(@class="new")]/@title',
 )
 
