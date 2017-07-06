055261a4199e3be530afcdeeaef178169cd4e5ea
Tony Bowden <tony@tmtm.com>
2017-07-06 08:05:50 +0100

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -7,7 +7,7 @@ es_2011 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://es.wikipedia.org/wiki/Composici%C3%B3n_de_la_Asamblea_Nacional_de_Venezuela_per%C3%ADodo_2011-2016',
   after: '//span[@id="Diputados_por_partido"]',
   before: '//span[@id="Hist.C3.B3rico_Directiva"]',
-  xpath: '//table[.//th[.="Diputado"]]//td[2]//a[not(@class="new")]/@title',
+  xpath: '//table[.//th[.="Diputado"]]//td[2]//a[not(@class="new")]/@title'
 )
 
 # Included as Ricardo Molina resigned mid term and was revised out of list
@@ -15,14 +15,21 @@ es_2015 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://es.wikipedia.org/w/index.php?title=Composici%C3%B3n_de_la_Asamblea_Nacional_de_Venezuela_per%C3%ADodo_2016-2021&oldid=89386527',
   after: '//span[@id="Diputados_por_partido"]',
   before: '//span[@id="Directiva"]',
-  xpath: '//table[.//th[contains(.,"Diputado")]]//td[2]//a[not(@class="new")]/@title',
+  xpath: '//table[.//th[contains(.,"Diputado")]]//td[2]//a[not(@class="new")]/@title'
 )
 
 es_2016 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://es.wikipedia.org/wiki/Composici%C3%B3n_de_la_Asamblea_Nacional_de_Venezuela_per%C3%ADodo_2016-2021',
   after: '//span[@id="Diputados_por_partido"]',
   before: '//span[@id="Directiva.2C_secretar.C3.ADa_y_jefaturas"]',
-  xpath: '//table[.//th[contains(.,"Diputado")]]//td[2]//a[not(@class="new")]/@title',
+  xpath: '//table[.//th[contains(.,"Diputado")]]//td[2]//a[not(@class="new")]/@title'
 )
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { es: es_2011 | es_2015 | es_2016 }, output: false)
+es_2016_notes = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://es.wikipedia.org/wiki/Composici%C3%B3n_de_la_Asamblea_Nacional_de_Venezuela_per%C3%ADodo_2016-2021',
+  after: '//span[@id="Diputados_por_partido"]',
+  before: '//span[@id="Directiva.2C_secretar.C3.ADa_y_jefaturas"]',
+  xpath: '//table[.//th[contains(.,"Diputado")]]//td[6]//a[not(@class="new")]/@title'
+)
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { es: es_2011 | es_2015 | es_2016 | es_2016_notes })
