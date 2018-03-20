de17b894df773c8c4d8d1795d1e98d1fef4c25d2
Tony Bowden <tony@tmtm.com>
2018-03-20 06:21:30 +0000

    Also fetch existing matches
    Don't lose anyone who's already matched

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,10 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require 'everypolitician'
 require 'wikidata/fetcher'
+
+existing = EveryPolitician::Index.new.country('Venezuela').lower_house.popolo.persons.map(&:wikidata).compact
 
 es_2011 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://es.wikipedia.org/wiki/Composici%C3%B3n_de_la_Asamblea_Nacional_de_Venezuela_per%C3%ADodo_2011-2016',
@@ -32,4 +35,4 @@ es_2016_notes = EveryPolitician::Wikidata.wikipedia_xpath(
   xpath: '//table[.//th[contains(.,"Diputado")]]//td[6]//a[not(@class="new")]/@title'
 )
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { es: es_2011 | es_2015 | es_2016 | es_2016_notes })
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { es: es_2011 | es_2015 | es_2016 | es_2016_notes })
